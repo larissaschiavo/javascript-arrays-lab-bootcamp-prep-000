2d68b585e48211ccd23eb1a47ae86be39d6e4fad
Larissa Schiavo <larissafschiavo@gmail.com>
2017-08-30 22:40:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much."
+  var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
+
   kittens.push(name);
 }
